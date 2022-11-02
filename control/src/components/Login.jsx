8cecfb9c58955c8fa1777ecf4f0c360d032669e4
danielmed <daniel.medina@talent.com>
2022-11-02 15:15:07 -0500

    changes in the login and styles

--- control/src/components/Login.jsx
+++ control/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import login from '../css/Login.module.css';
 
 const Login = () => {
 
@@ -8,8 +9,23 @@ const sesion = () =>{
 }
 
   return (
-    <div>Login ....
-        <button onClick={sesion}>Login</button>
+    <div className={login}>
+     <div className='main-wrap'>
+      <form>
+      <div className="sas-title">SAS Login</div>
+        <label >Name</label>
+        <div>
+          <img src='https://cdn-dynamic.talent.com/ajax/img/get-svg.php?icon=mail_icon&size=24&color=661D72' />
+        <input type='text'></input>
+        </div>
+        <label >Password</label>
+        <div>
+          <img src='https://cdn-dynamic.talent.com/ajax/img/get-svg.php?icon=lock&size=24&color=661D72' />
+        <input type='password'></input>
+        </div>
+        <button onClick={sesion} className={login.sasMainAction }>Login</button>
+      </form>
+      </div>
     </div>
   )
 }
