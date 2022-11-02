af635279a6de24cfd4dd58824987f0eaf4c79d4c
danielmed <daniel.medina@talent.com>
2022-11-02 16:31:49 -0500

    login changes, style changes, delete some style file that we dont use anymore

--- control/src/components/Login.jsx
+++ control/src/components/Login.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import login from '../css/Login.module.css';
+import React from "react";
+import login from "../css/Login.module.css";
+//import "../css/common.css";
+
 
 const Login = () => {
-
-const sesion = () =>{
-    window.sessionStorage.setItem("Logged","True");
+  const sesion = () => {
+    window.sessionStorage.setItem("Logged", "True");
     window.location.reload();
-}
+  };
 
   return (
-    <div className={login}>
-     <div className='main-wrap'>
-      <form>
-      <div className="sas-title">SAS Login</div>
-        <label >Name</label>
-        <div>
-          <img src='https://cdn-dynamic.talent.com/ajax/img/get-svg.php?icon=mail_icon&size=24&color=661D72' />
-        <input type='text'></input>
-        </div>
-        <label >Password</label>
-        <div>
-          <img src='https://cdn-dynamic.talent.com/ajax/img/get-svg.php?icon=lock&size=24&color=661D72' />
-        <input type='password'></input>
-        </div>
-        <button onClick={sesion} className={login.sasMainAction }>Login</button>
-      </form>
-      </div>
-    </div>
-  )
-}
+   
+      <div className={login.mainWrap}>
+        <form method="post">
+          <div className={login.sasTitle}>SAS Login</div>
 
-export default Login
+          <div className={login.sasInputHolder}>
+            <div className={login.sasInputLabel}>Email</div>
+            <div className={login.sasInputText}>
+              <img classname={login.sasInputIcon} src="https://cdn-dynamic.talent.com/ajax/img/get-svg.php?icon=mail_icon&size=24&color=661D72" />
+              <input className={login.sasInput} type="text"></input>
+            </div>
+          </div>
+          <div className={login.sasInputHolder}>
+            <div className={login.sasInputLabel}>Password</div>
+            <div className={login.sasInputText}>
+              <img classname={login.sasInputIcon} src="https://cdn-dynamic.talent.com/ajax/img/get-svg.php?icon=lock&size=24&color=661D72" />
+              <input className={login.sasInput} type="password"></input>
+            </div>
+          </div>
+
+
+
+
+
+          
+          <button onClick={sesion} className={login.sasMainAction}>
+            Login
+          </button>
+        </form>
+      </div>
+    
+  );
+};
+
+export default Login;
